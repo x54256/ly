37c585f0d98a2731387038d1e8a04f9bbf47f4d9
yujx <yujx@dist.com.cn>
2018-11-22 16:15:31 +0800

    @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)

--- ly-item/ly-item-service/src/main/java/cn/x5456/leyou/LyItemService.java
+++ ly-item/ly-item-service/src/main/java/cn/x5456/leyou/LyItemService.java
@@ -2,12 +2,13 @@ package cn.x5456.leyou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 主启动类位置要注意，为了扫描@ControlledAdvice注解，实现异常处理
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableDiscoveryClient
 public class LyItemService {
     public static void main(String[] args) {
